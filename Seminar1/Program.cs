8d1aa484a5becf1507f7dc14c7976d68f70ab136
Ksenia <ksenia.barteva@gmail.com>
2022-08-23 19:48:53 +0700

    Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя
+Console.Write("Input number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine("Tne quad of " + number + " is " + kvadrat);
+
+
+ 
