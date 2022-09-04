308157c69eddbf944d5c8510d1899abb88d09ce8
Ksenia <ksenia.barteva@gmail.com>
2022-09-04 22:56:14 +0700

    Seminar4

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -82,7 +82,7 @@ Console.WriteLine($"Произведение чисел от 1 до {number} = {
 /*
 int[] CreateRandomArray (int size, int minValue, int maxValue)
 {
-    int newArray = new int[size];
+    int[] newArray = new int[size];
 
     for(int i = 0; i < size; i++)
         newArray[i] = new Random().Next(minValue, maxValue + 1);
