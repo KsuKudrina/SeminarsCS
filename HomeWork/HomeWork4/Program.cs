9ccbe2a5646ac2ff00be527c263b1753bcabfaf6
Ksenia <ksenia.barteva@gmail.com>
2022-09-04 22:49:55 +0700

    HomeWork4

---
+++ HomeWork/HomeWork4/Program.cs
@@ -0,0 +1,79 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+/*
+int DegreeOfNumber(int numA, int numB)
+{
+    int current = numA;
+    
+    for(int count = 2; count <= numB; count++)
+    {
+        current *= numA; 
+    }
+    return current;
+}
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {a} в натуральной степени числа {b} = {DegreeOfNumber(a, b)}");
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+/*
+void MethodSum (int num)
+{
+    int n = 0;
+    int sum = 0;
+    while(num > 0)
+    {
+        n = num % 10;
+        num /= 10;
+        sum += n; 
+    }
+    Console.WriteLine("Сумма  цифр в числе = " + sum);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+MethodSum(number);
+*/
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+ 
+/*
+int[] CreateArray (int size, int arg)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    return newArray;   
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();      
+}
+
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Заполните массив: ");
+int arg = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(size, arg);
+ShowArray(myArray);
+*/
---
+++ HomeWork/HomeWork5/Program.cs
@@ -0,0 +1,165 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//  [345, 897, 568, 234] -> 2
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return newArray;
+}
+
+void ShowArray (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int EvenNumbers(int[] array)
+{
+    int quantity = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+            quantity++;
+    }
+    return quantity;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input min possible positive three-digit value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input max possible positive three-digit value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+
+Console.WriteLine($"Number of even numbers is {EvenNumbers(myArray)}");
+*/
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+//  [3, 7, 23, 12] -> 19
+//  [-4, -6, 89, 6] -> 0
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return newArray;
+}
+
+void ShowArray (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int MethodSum (int[] array)
+{
+    int sum = 0;
+    
+    for(int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }    
+    return sum;
+}    
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input min posible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input max posible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+
+Console.WriteLine($"Sum of elements in odd positions is {MethodSum(myArray)}");
+*/
+
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+//  [3 7 22 2 78] -> 76
+
+double[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    double[] newArray = new double[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+    }
+    return newArray;
+}
+
+void ShowArray (double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double NumberMax(double[] array)
+{
+    double max = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+    }
+    return max;
+}
+
+double NumberMin(double[] array)
+{    
+    int i = 0;
+    double min = array[i];
+    
+    while(i < array.Length)
+    {
+        if(array[i] < min)
+            min = array[i];
+    }
+    return min;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input min posible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input max posible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+
+double MaxNum = NumberMax(myArray);
+double MinNum = NumberMin(myArray);
+Console.WriteLine(MaxNum);
+Console.WriteLine(MinNum);
+
+
+Console.WriteLine($"Difference between  maximum and minimum element is {MaxNum - MinNum}");
