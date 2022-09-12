a94ac7c55e03be261cc5f2182eed0a9569faa21d
Ksenia <ksenia.barteva@gmail.com>
2022-09-12 18:54:56 +0700

    Seminar8

---
+++ Seminar8/Program.cs
@@ -0,0 +1,264 @@
+﻿//Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of colums: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);   
+        }
+    }
+    
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void ChangeRows(int[,] array, int row1, int row2)
+{
+    if(row1 >= 0 && row1 < array.GetLength(0) && row2 >= 0 && row2 < array.GetLength(0) && row1 != row2)
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = array[row1,j];
+            array[row1, j] = array[row2, j];
+            array[row2, j] = temp;
+        }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+ChangeRows(myArray, 0, 5);
+Show2dArray(myArray);
+*/
+//Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+//В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of colums: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);   
+        }
+    }
+    
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void SwapRC(int[,] array)
+{
+    if(array.GetLength(0) != array.GetLength(1))
+        Console.WriteLine($"Замена строк на столбцы невозможна");
+    else
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = i + 1; j < array.GetLength(1); j++)
+                {
+                    int temp = array[i,j];
+                    array[i, j] = array[j, i];
+                    array[j, i] = temp; 
+                }
+        }
+
+    Console.WriteLine();
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+SwapRC(myArray);
+Show2dArray(myArray);
+
+
+//Из двумерного массива целых чисел удалить строку и столбец, 
+//на пересечении которых расположен наименьший элемент.
+*/
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of colums: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);   
+        }
+    }
+    
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int[,] CopedArray(int[,] array)
+{
+   int rows = array.GetLength(0);
+   int columns = array.GetLength(1);
+   int[,] newArray2 = new int[rows - 1, columns - 1];
+   int min = array[0,0];
+   int minI = 0;
+   int minJ = 0;
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(min > array[i, j])
+            {
+                min = array[i, j];
+                minI = i;
+                minJ = j;
+            }      
+        }
+    }
+    int m = 0;
+    for(int i = 0; i < rows; i++)
+    {
+        if(i == minI) continue;
+        int n = 0;
+        for(int j = 0; j < columns; j++)
+        {
+            if(j == minJ) continue;
+            newArray2[m, n] = array[i, j];
+            n++;
+        }
+        m++;
+    }
+    return newArray2;
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+int[,] myArray2 = CopedArray(myArray);
+Show2dArray(myArray2);
+//Написать программу поэлементного копирования массива.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of colums: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);   
+        }
+    }
+    
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void CopedArray(int[,] array)
+{
+   int[,] newArray2 = new int[array.GetLength(0), array.GetLength(1)];
+   for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray2[i, j] = array[i, j];
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+CopedArray(myArray);
+Show2dArray(myArray);
+*/
+
+//Домашняя MathProfi
